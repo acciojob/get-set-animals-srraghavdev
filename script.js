d80706570237db803ec03c2a7686ec1c88a698df
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-15 08:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ class Animal {
 
 class Dog extends Animal {
 	bark(){
-		console.log("woof")
+		console.log(`The ${this.species} makes a sound`)
 	}
 }
 
 class Cat extends Animal {
 	purr(){
-		console.log("purr")
+		console.log(`The ${this.species} makes a sound`)
 	}
 }
 
